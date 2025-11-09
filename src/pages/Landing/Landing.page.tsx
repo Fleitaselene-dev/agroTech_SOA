4edcb6a911e32c3145becf7d89fefbc05872a4a9
Ailin Ayelen Miño <pukyailin@gmail.com>
2025-11-09 17:51:51 -0300

    corrigiendo errores de estilos en landing

--- src/pages/Landing/Landing.page.tsx
+++ src/pages/Landing/Landing.page.tsx
@@ -1,4 +1,3 @@
-
 import { Link } from "wouter";
 import Navbar from "../../assets/components/layout/Navbar";
 import HeroLanding from "../../assets/components/landingComp/HeroLanding";
@@ -6,14 +5,13 @@ import Footer from "../../assets/components/layout/Footer";
 import Servicios from "../../assets/components/landingComp/Servicios";
 import ImpactSection from "../../assets/components/landingComp/ImpacSeccion";
 
-import { FeaturesSection } from '../../assets/components/landingComp/FeaturesSeccion';
+import { FeaturesSection } from "../../assets/components/landingComp/FeaturesSeccion";
 
-
-export default function Landing  () {
+export default function Landing() {
   return (
     <div>
       <nav>
-        <Navbar/>
+        <Navbar />
       </nav>
       <section>
         <HeroLanding />
@@ -27,7 +25,6 @@ export default function Landing  () {
       <section>
         <Servicios />
       </section>
-    
 
       <section className="py-20">
         <div className="max-w-4xl mx-auto px-4 text-center">
@@ -48,12 +45,12 @@ export default function Landing  () {
                 text-[#507d2a] transition-all duration-1500 ease-in-out 
                 hover:shadow-[1px_1px_200px_#252525] hover:text-white hover:border-none
                 after:content-[''] after:absolute after:left-0 after:top-0 
-                after:h-[10px] after:w-[10px] after:rounded-[30px] after:bg-[#3c661a]
+                after:h-2.5 after:w-2.5 after:rounded-[30px] after:bg-[#3c661a]
                 after:invisible after:z-[-1] after:transition-all after:duration-1500 after:ease-in-out
-                hover:after:visible hover:after:scale-[80] hover:after:translate-x-[2px]
+                hover:after:visible hover:after:scale-[80] hover:after:translate-x-0.5
               "
             >
-              <span className="z-[1] font-extrabold tracking-[1px]">Empezar</span>
+              <span className="z-1 font-extrabold tracking-[1px]">Empezar</span>
             </Link>
           </div>
         </div>
@@ -64,6 +61,4 @@ export default function Landing  () {
       </section>
     </div>
   );
-};
-
-
+}
